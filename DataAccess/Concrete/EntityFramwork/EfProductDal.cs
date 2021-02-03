f682c0268aa70cf4ba462e8c748cabaaefb9d343
deniz <deniz@DESKTOP-PGJR05G>
2021-02-03 23:20:49 +0300

    03.02.2021

---
+++ DataAccess/Concrete/EntityFramwork/EfProductDal.cs
@@ -0,0 +1,60 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramwork
+{
+    public class EfProductDal : IProductDal
+    {
+
+        public void Add(Product entity)
+        {
+
+            using (NorthwindContext context=new NorthwindContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public Product Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            using (NorthwindContext context =new NorthwindContext())
+            {
+
+                return filter==null?context.Set<Product>().ToList():context.Set<Product>().Where(filter).ToList();
+            }   
+        }
+
+        public void Update(Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var uptadedEntity = context.Entry(entity);
+                uptadedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramwork/IEfCategoryDal.cs
@@ -0,0 +1,37 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramwork
+{
+    public class IEfCategoryDal : ICategoryDal
+    {
+        public void Add(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAll(Expression<Func<Category, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
